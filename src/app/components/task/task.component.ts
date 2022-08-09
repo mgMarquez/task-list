215a49412b2dbb3c378f6e5a254d29da719c828d
Mauricio Marquez <mgmarquezsilva@gmail.com>
2022-08-09 15:52:23 -0300

    servicio y servidor de tareas listo

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {Task} from '../../Task';
-import {TASKS} from '../../mock-task';
-
+import { TaskService } from '../../service/task.service';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+
+  ) { }
 
   ngOnInit(): void {
+    // Like Promise
+    this.taskService.getTasks().subscribe( tasks => {
+
+      this.tasks = tasks;
+    });
   }
 
 }
--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -9,13 +9,13 @@ export const TASKS: Task[] = [
     },
     {
         id: 2,
-        text: "Hacer las compras para la centa",
+        text: "Hacer las compras para la cena",
         day: 'Agosto 5 a las 17:00',
         reminder: true
     },
     {
         id: 3,
-        text: "Investigar sobre Boostrap",
+        text: "Investigar sobre Bootstrap",
         day: 'Agosto 5 a las 19:00',
         reminder: false
     },
